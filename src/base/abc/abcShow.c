313ecc34f43bc36b521a00405a3fbf0fb3633b43
chiux <xuyang1024@gmail.com>
2020-10-13 18:18:59 +0800

    finish Part2

--- src/base/abc/abcShow.c
+++ src/base/abc/abcShow.c
@@ -358,11 +358,11 @@ void Abc_ShowFile( char * FileNameDot )
 
     // create the PostScript file name
     FileGeneric = Extra_FileNameGeneric( FileNameDot );
-    sprintf( FileNamePs,  "%s.pdf",  FileGeneric ); 
+    sprintf( FileNamePs,  "%s.ps",  FileGeneric ); 
     ABC_FREE( FileGeneric );
 
     // generate the PostScript file using DOT
-    sprintf( CommandDot,  "%s -Tpdf -o %s %s", pDotName, FileNamePs, FileNameDot ); 
+    sprintf( CommandDot,  "%s -Tps -o %s %s", pDotName, FileNamePs, FileNameDot ); 
     RetValue = system( CommandDot );
     if ( RetValue == -1 )
     {
