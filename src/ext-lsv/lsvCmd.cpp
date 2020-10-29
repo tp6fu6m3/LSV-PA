0a0d58aebb21d009a07743ee079f593c0cacb60e
Shao-Wei <39191214+Shao-Wei@users.noreply.github.com>
2020-10-29 13:34:01 +0800

    add command lsv_print_sopunate (#183)
    * finish sopunate command
    
    * bug fix in parsing pData
    
    * remove dummy output when node has no fanin

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,60 +1,175 @@
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
+#include <vector>
+#include <algorithm>
 
-static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
+ABC_NAMESPACE_IMPL_START
 
-void init(Abc_Frame_t* pAbc) {
-  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
+using namespace std;
+
+static Abc_Ntk_t * DummyFunction( Abc_Ntk_t * pNtk )
+{
+    Abc_Print( -1, "Please rewrite DummyFunction() in file \"icompactCommand.cc\".\n" );
+    return NULL;
 }
 
-void destroy(Abc_Frame_t* pAbc) {}
+static int sopunate_Command( Abc_Frame_t_ * pAbc, int argc, char ** argv )
+{
+    int result       = 0;
+    int c            = 0;
+    int fVerbose     = 0;
+    
+    Abc_Ntk_t *pNtk;
+    Abc_Obj_t *pObj, *pFanin;
+    int i, j, k;
+    char *buff, *t;
 
-Abc_FrameInitializer_t frame_initializer = {init, destroy};
-
-struct PackageRegistrationManager {
-  PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
-} lsvPackageRegistrationManager;
-
-void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
-  Abc_Obj_t* pObj;
-  int i;
-  Abc_NtkForEachNode(pNtk, pObj, i) {
-    printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
-    Abc_Obj_t* pFanin;
-    int j;
-    Abc_ObjForEachFanin(pObj, pFanin, j) {
-      printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
-             Abc_ObjName(pFanin));
+    vector<int> vecInput, vecPcount, vecNcount, vecPos, vecNeg, vecBi;
+    bool fOutput;
+ 
+    Extra_UtilGetoptReset();
+    while ( ( c = Extra_UtilGetopt( argc, argv, "vh" ) ) != EOF )
+    {
+        switch ( c )
+        {            
+            case 'v':
+                fVerbose ^= 1;
+                break;
+            case 'h':
+                goto usage;
+            default:
+                goto usage;
+        }
     }
-    if (Abc_NtkHasSop(pNtk)) {
-      printf("The SOP of this node:\n%s", (char*)pObj->pData);
-    }
-  }
-}
 
-int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
-  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
-  int c;
-  Extra_UtilGetoptReset();
-  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
-    switch (c) {
-      case 'h':
-        goto usage;
-      default:
-        goto usage;
+    pNtk = Abc_FrameReadNtk(pAbc);
+    if(!Abc_NtkIsSopLogic(pNtk))
+    {
+        printf("Error: input network is not in sop format.\n");
+        return 1;
     }
-  }
-  if (!pNtk) {
-    Abc_Print(-1, "Empty network.\n");
-    return 1;
-  }
-  Lsv_NtkPrintNodes(pNtk);
-  return 0;
 
+    Abc_NtkForEachNode(pNtk, pObj, i)
+    {
+        vecInput.clear();
+        vecPcount.clear();
+        vecNcount.clear();
+        vecPos.clear();
+        vecNeg.clear();
+        vecBi.clear();
+
+        Abc_ObjForEachFanin(pObj, pFanin, j)
+        {
+            vecInput.push_back(Abc_ObjId(pFanin));
+            vecPcount.push_back(0);
+            vecNcount.push_back(0);
+        }
+
+        if(vecInput.size() == 0)
+            continue;
+
+        buff = (char*)pObj->pData;
+        t = strtok(buff, "\n");
+        fOutput = (t[vecInput.size()+1] == '1');
+        while(true)
+        {
+            for(k=0; k<vecInput.size(); k++)
+            {
+                if(t[k] == '1')
+                    vecPcount[k]++;
+                if(t[k] == '0')
+                    vecNcount[k]++;
+            }
+            t = strtok(NULL, "\n");
+            if(t == NULL)
+                break;
+        }
+
+        for(k=0; k<vecInput.size(); k++)
+        {
+            if(fOutput && (vecNcount[k] == 0))
+                vecPos.push_back(vecInput[k]);
+            if(!fOutput && (vecPcount[k] == 0))
+                vecPos.push_back(vecInput[k]);
+            if(fOutput && (vecPcount[k] == 0))
+                vecNeg.push_back(vecInput[k]);
+            if(!fOutput && (vecNcount[k] == 0))
+                vecNeg.push_back(vecInput[k]);
+            if((vecNcount[k]!=0) && (vecPcount[k]!=0))
+                vecBi.push_back(vecInput[k]);            
+        }
+
+        printf("node %s:\n", Abc_ObjName(pObj));
+        if(vecPos.size())
+        {
+            sort(vecPos.begin(), vecPos.end());
+            printf("+unate inputs: ");
+            for(k=0; k<vecPos.size(); k++)
+            {
+                if(k!=0)
+                    printf(",");
+                printf("%s", Abc_ObjName(Abc_NtkObj(pNtk, vecPos[k])));
+            }
+            printf("\n");
+        }
+        if(vecNeg.size())
+        {
+            sort(vecNeg.begin(), vecNeg.end());
+            printf("-unate inputs: ");
+            for(k=0; k<vecNeg.size(); k++)
+            {
+                if(k!=0)
+                    printf(",");
+                printf("%s", Abc_ObjName(Abc_NtkObj(pNtk, vecNeg[k])));
+            }
+            printf("\n");
+        }
+        if(vecBi.size())
+        {
+            sort(vecBi.begin(), vecBi.end());
+            printf("binate inputs: ");
+            for(k=0; k<vecBi.size(); k++)
+            {
+                if(k!=0)
+                    printf(",");
+                printf("%s", Abc_ObjName(Abc_NtkObj(pNtk, vecBi[k])));
+            }
+            printf("\n");
+        }
+    }
+    return result;
+    
 usage:
-  Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
-  Abc_Print(-2, "\t        prints the nodes in the network\n");
-  Abc_Print(-2, "\t-h    : print the command usage\n");
-  return 1;
+    Abc_Print( -2, "usage: lsv_print_sopunate [options]\n" );
+    Abc_Print( -2, "\t              prints the unate information for each node\n" );
+    Abc_Print( -2, "\t-v            : verbosity [default = %d]\n", fVerbose );
+    Abc_Print( -2, "\t-h            : print the command usage\n" );
+    return 1;   
 }
+
+// called during ABC startup
+static void init(Abc_Frame_t* pAbc)
+{ 
+    Cmd_CommandAdd( pAbc, "AlCom", "lsv_print_sopunate", sopunate_Command, 1);
+}
+
+// called during ABC termination
+static void destroy(Abc_Frame_t* pAbc)
+{
+}
+
+// this object should not be modified after the call to Abc_FrameAddInitializer
+static Abc_FrameInitializer_t sopunate_frame_initializer = { init, destroy };
+
+// register the initializer a constructor of a global object
+// called before main (and ABC startup)
+struct sopunate_registrar
+{
+    sopunate_registrar() 
+    {
+        Abc_FrameAddInitializer(&sopunate_frame_initializer);
+    }
+} sopunate_registrar_;
+
+ABC_NAMESPACE_IMPL_END
--- src/ext-lsv/module.make
+++ src/ext-lsv/module.make
@@ -1,2 +1,1 @@
-SRC += \
-    src/ext-lsv/lsvCmd.cpp
+SRC += src/ext-lsv/lsvCmd.cpp
