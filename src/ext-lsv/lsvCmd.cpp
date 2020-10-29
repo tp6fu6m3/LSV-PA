a3f5535a357c77b8df9a4757acb16b01ccc4c20a
tp6fu6m3 <m3tp6fu6@gmail.com>
2020-10-29 13:35:48 +0800

    submit pa1-part3 with cmd lsv_print_sopunate (#180)
    * LSV-PA1 part1&part2
    
    * LSV-PA1 part3
    
    * Add files via upload
    
    * Delete lsvCmd.cpp
    
    * Add files via upload

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,11 +1,18 @@
+/****************************************************************************
+  FileName     [ lsvCmd.cpp ]
+  PackageName  [ print_sopunate ]
+  Synopsis     [ Define print_sopunate commands ]
+  Author       [ Qi-Yu Chen (r07943034) ]
+  Copyright    [ Copyleft(c) Alcom, GIEE, NTU, Taiwan ]
+ ****************************************************************************/
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
 
-static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_CommandPrintSopUnate(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
-  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_sopunate", Lsv_CommandPrintSopUnate, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -16,24 +23,84 @@ struct PackageRegistrationManager {
   PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
 } lsvPackageRegistrationManager;
 
-void Lsv_NtkPrintNodes(Abc_Ntk_t* pNtk) {
-  Abc_Obj_t* pObj;
+void PrintUnateInfo(char* str, Vec_Ptr_t* vec){
+  Abc_Obj_t* pEntry;
   int i;
-  Abc_NtkForEachNode(pNtk, pObj, i) {
-    printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
-    Abc_Obj_t* pFanin;
-    int j;
-    Abc_ObjForEachFanin(pObj, pFanin, j) {
-      printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
-             Abc_ObjName(pFanin));
-    }
-    if (Abc_NtkHasSop(pNtk)) {
-      printf("The SOP of this node:\n%s", (char*)pObj->pData);
-    }
+  
+  if(Vec_PtrSize(vec) == 0)
+    return;
+  printf("%s: ", str);
+  Vec_PtrForEachEntry(Abc_Obj_t* , vec, pEntry, i){
+    printf("%s", Abc_ObjName(pEntry));
+    if(i == Vec_PtrSize(vec) - 1)
+      printf("%c", '\n');
+    else
+      printf("%c", ',');
   }
 }
 
-int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
+static int Vec_PtrSortCompare(Abc_Obj_t** pp1, Abc_Obj_t** pp2){
+  return Abc_ObjId(*pp1) > Abc_ObjId(*pp2);
+}
+
+void Lsv_NtkPrintSopUnate(Abc_Ntk_t* pNtk){
+  Abc_Obj_t* pObj;
+  int i;
+
+  Abc_NtkForEachNode(pNtk, pObj, i){
+    char* pSop = (char*)pObj->pData;
+    int nFanins = Abc_ObjFaninNum(pObj);
+    char* pCube;
+    bool unateTable[nFanins][2] = {0};
+    
+    Abc_SopForEachCube(pSop, nFanins, pCube){
+      char Value;
+      int j;
+      bool isOnset = *(pCube + nFanins + 1) - '0';
+      
+      Abc_CubeForEachVar(pCube, Value, j){
+        if(Value == '-')
+          continue;
+        if(Value - '0' == isOnset)
+          unateTable[j][1] = 1;
+        else
+          unateTable[j][0] = 1;
+      }
+    }
+    
+    if(nFanins == 0)
+      continue;
+    Vec_Ptr_t* Vec_posUnate = Vec_PtrAlloc(nFanins);
+    Vec_Ptr_t* Vec_negUnate = Vec_PtrAlloc(nFanins);
+    Vec_Ptr_t* Vec_binate = Vec_PtrAlloc(nFanins);
+    
+    Abc_Obj_t* pFanin;
+    int k;
+    Abc_ObjForEachFanin(pObj, pFanin, k){
+      if(!unateTable[k][0])
+        Vec_PtrPush(Vec_posUnate, pFanin);
+      if(!unateTable[k][1])
+        Vec_PtrPush(Vec_negUnate, pFanin);
+      if(unateTable[k][0] && unateTable[k][1])
+        Vec_PtrPush(Vec_binate, pFanin);
+    }
+
+    Vec_PtrSort(Vec_posUnate, (int (*)()) Vec_PtrSortCompare);
+    Vec_PtrSort(Vec_negUnate, (int (*)()) Vec_PtrSortCompare);
+    Vec_PtrSort(Vec_binate, (int (*)()) Vec_PtrSortCompare);
+
+    printf("node %s:\n", Abc_ObjName(pObj));
+    PrintUnateInfo("+unate inputs", Vec_posUnate);
+    PrintUnateInfo("-unate inputs", Vec_negUnate);
+    PrintUnateInfo("binate inputs", Vec_binate);
+
+    Vec_PtrFree(Vec_posUnate);
+    Vec_PtrFree(Vec_negUnate);
+    Vec_PtrFree(Vec_binate);
+  }
+}
+
+int Lsv_CommandPrintSopUnate(Abc_Frame_t* pAbc, int argc, char** argv) {
   Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
   int c;
   Extra_UtilGetoptReset();
@@ -49,12 +116,12 @@ int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
     Abc_Print(-1, "Empty network.\n");
     return 1;
   }
-  Lsv_NtkPrintNodes(pNtk);
+  Lsv_NtkPrintSopUnate(pNtk);
   return 0;
 
 usage:
-  Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
-  Abc_Print(-2, "\t        prints the nodes in the network\n");
+  Abc_Print(-2, "usage: lsv_print_sopunate [-h]\n");
+  Abc_Print(-2, "\t        prints the unate information for each node\n");
   Abc_Print(-2, "\t-h    : print the command usage\n");
   return 1;
 }
